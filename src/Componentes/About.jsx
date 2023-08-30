625f01764df28e1c2eef70000ad2fcaced3846f4
Juan Sebastian Gonzalez Sossa <juan.gonzalez@konecta-group.com>
2023-08-30 11:05:13 -0500

    Cambios de emergencia

--- src/Componentes/About.jsx
+++ src/Componentes/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { motion } from "framer-motion";
 import '../Styles/About.css';
 
 export const About = () => {
   return (
     <section className='about'>
       <div className='about-bx'>
+        <motion.div
+          className="about-title"
+        >
+          About me
+        </motion.div>
       </div>
     </section>
   )
